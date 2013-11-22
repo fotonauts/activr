27e1e52014969d1db6c3196f5df984d0439ae5d2
Aymerick <aymerick@fotonauts.com>
2013-11-22 14:25:33 +0100

    Fix railtie

--- lib/activr/railtie.rb
+++ lib/activr/railtie.rb
@@ -1,12 +1,12 @@
 module Activr
-  class Railtie < Rails::Railtie
 
-    initializer "activr.configure_rails_initialization" do
+  class Railtie < Rails::Railtie
+    initializer "activr.configure_rails_initialization", :before => :set_autoload_paths do |app|
       Activr.config.app_path = File.join(Rails.root, 'app')
 
-      config.autoload_paths << File.join(Activr.config.app_path, 'activities')
-      config.autoload_paths << File.join(Activr.config.app_path, 'timelines')
+      app.config.autoload_paths << File.join(Activr.config.app_path, 'activities')
+      app.config.autoload_paths << File.join(Activr.config.app_path, 'timelines')
     end
-
   end
+
 end
