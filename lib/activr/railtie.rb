73bd33ccbdf97312181f28ff21ceee2e972d6d9e
Aymerick <aymerick@fotonauts.com>
2013-11-25 21:00:15 +0100

    railtie: set mongodb conf from mongoid

--- lib/activr/railtie.rb
+++ lib/activr/railtie.rb
@@ -1,11 +1,22 @@
 module Activr
 
   class Railtie < Rails::Railtie
-    initializer "activr.configure_rails_initialization", :before => :set_autoload_paths do |app|
+    initializer "activr.configure_autoload", :before => :set_autoload_paths do |app|
       Activr.config.app_path = File.join(Rails.root, 'app')
 
       app.config.autoload_paths << File.join(Activr.config.app_path, 'activities')
       app.config.autoload_paths << File.join(Activr.config.app_path, 'timelines')
+    end
+
+    initializer "activr.set_conf", :after => 'mongoid.load-config' do |app|
+      Activr.configure do |config|
+        # @todo Remove that when async workers system is ready
+        config.sync = true
+
+        if Mongoid.sessions[:default] && !Mongoid.sessions[:default][:database].blank? && !Mongoid.sessions[:default][:hosts].blank?
+          config.mongodb[:uri] = "mongodb://#{Mongoid.sessions[:default][:hosts].first}/#{Mongoid.sessions[:default][:database]}"
+        end
+      end
     end
   end
 
