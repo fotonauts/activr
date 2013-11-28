a9a38c031084b11d4492109c6b1a5f4cf07051b3
Aymerick <aymerick@fotonauts.com>
2013-11-28 11:06:10 +0100

    Better railtie initialization

--- lib/activr/railtie.rb
+++ lib/activr/railtie.rb
@@ -1,15 +1,9 @@
 module Activr
 
   class Railtie < Rails::Railtie
-    initializer "activr.configure_autoload", :before => :set_autoload_paths do |app|
-      Activr.config.app_path = File.join(Rails.root, 'app')
-
-      app.config.autoload_paths << File.join(Activr.config.app_path, 'activities')
-      app.config.autoload_paths << File.join(Activr.config.app_path, 'timelines')
-    end
-
     initializer "activr.set_conf", :after => 'mongoid.load-config' do |app|
       Activr.configure do |config|
+        config.app_path = File.join(Rails.root, 'app')
         # @todo Remove that when async workers system is ready
         config.sync = true
 
@@ -17,6 +11,11 @@ module Activr
           config.mongodb[:uri] = "mongodb://#{Mongoid.sessions[:default][:hosts].first}/#{Mongoid.sessions[:default][:database]}"
         end
       end
+    end
+
+    config.after_initialize do |app|
+      app.config.paths.add('app/activities', :eager_load => true)
+      app.config.paths.add('app/timelines',  :eager_load => true)
     end
 
     config.after_initialize do
